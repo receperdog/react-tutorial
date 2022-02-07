9f6d524bcf03d8975109a7a60bf55fe85c9b83f9
Recep Tayyip Erdogan <66259609+rtayyiperdogan@users.noreply.github.com>
2022-02-07 20:50:20 +0300

    new component edded

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,11 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+  const clickHandler = () => {
+    title = "changed";
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +15,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
