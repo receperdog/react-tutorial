6acd41b15e43234e8f92d87e9f10f5a40b8caa48
Recep Tayyip Erdogan <66259609+rtayyiperdogan@users.noreply.github.com>
2022-02-08 03:24:44 +0300

    errors fixed

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,8 +2,10 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
-  <div className="new-expense">
-    <ExpenseForm />
-  </div>;
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
 };
 export default NewExpense;
